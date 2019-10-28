d59e7eb87e540df59c269a2e083297c5e3680b94
zke <1160345031@qq.com>
2019-10-28 17:32:33 +0800

    feat: 增加自定义样式tabbar

--- lib/pages/member.dart
+++ lib/pages/member.dart
@@ -7,28 +7,35 @@ class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back,
+                semanticLabel: 'Close',
+              ),
+              onPressed: () {
+               Application.router.navigateTo(context, "/cart");
+              }),
+          title: const Text('Detail'),
+        ),
         body: Center(
-      child: Column(
-        children: <Widget>[      Number(),
-           MyButtonToMember(),
-           MyButton()],
-      ),
-    ));
+          child: Column(
+            children: <Widget>[Number(), MyButtonToMember(), MyButton()],
+          ),
+        ));
   }
 }
 
 class Number extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top:200),
+      margin: EdgeInsets.only(top: 200),
       child: Provide<Counter>(
-        builder: (context,child,counter){
+        builder: (context, child, counter) {
           return Text(
             '${counter.value}',
-             style: Theme.of(context).textTheme.display1,
+            style: Theme.of(context).textTheme.display1,
           );
         },
       ),
@@ -36,30 +43,28 @@ class Number extends StatelessWidget {
   }
 }
 
-
 class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:RaisedButton(
-        onPressed: (){
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('递增'),
-      )
-    );
+        child: RaisedButton(
+      onPressed: () {
+        Provide.value<Counter>(context).increment();
+      },
+      child: Text('递增'),
+    ));
   }
 }
+
 class MyButtonToMember extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:RaisedButton(
-        onPressed: (){
-          Application.router.navigateTo(context,"/cart");
-        },
-        child: Text('跳转cart'),
-      )
-    );
+        child: RaisedButton(
+      onPressed: () {
+        Application.router.navigateTo(context, "/cart");
+      },
+      child: Text('跳转cart'),
+    ));
   }
 }
